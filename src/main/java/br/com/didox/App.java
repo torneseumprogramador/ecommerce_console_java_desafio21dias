9c7a77ddc510139590eb9f93b914921bfe407663
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-10-06 06:12:50 -0300

    primeiro exercicio

--- src/main/java/br/com/didox/App.java
+++ src/main/java/br/com/didox/App.java
@@ -1,13 +1,54 @@
 package br.com.didox;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+ 
         /*
-        mostre()
-        leia()
+         * Exercicio 1
+         * João é um professor de matemática e precisa de um programa
+         * que faça o calculo da seguinte expressão:
+         * (x * 8 + y) - x + y / 100
+         * faça um programa que resolva o problema da expressão que o João 
+         * precisa.
+         * 
+         * LEMBRE-SE: pense na usabilidade !!!
+         */
+
+
+
+
+
+
+        /*
+        System.out.println("Digite um número");
+        int numero =  Integer.parseInt(reader.readLine());
+        var resultado = numero + 1000;
+        System.out.println("O resultado de (" + numero + ") + 1000 é de : " + resultado);
         */
+
+
+
+        /* 
+        // mostre()
+        System.out.println("Digite o seu nome");
+
+        // leia()
+        var nome = reader.readLine();
+
+        // mostre() mostrando dados concatenando variável nome capturada
+        System.out.println("O nome digitado foi: " + nome);
+        */
+
+
+        /*
+        String name = reader.readLine();
         
         int numero1 = 3;
         var numero2 = 5;
@@ -16,5 +57,6 @@ public class App
 
         // mostre() # mostra informação na tela
         System.out.println( "O resultado da operação é " + soma );
+        */
     }
 }
