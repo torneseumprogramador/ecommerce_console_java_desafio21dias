978f6de955d08c9bda178e1d53f2c6f263a8bae6
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-10-06 05:41:48 -0300

    forçando aplicação para rodar no java 11

--- src/main/java/br/com/didox/App.java
+++ src/main/java/br/com/didox/App.java
@@ -1,13 +1,20 @@
 package br.com.didox;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        /*
+        mostre()
+        leia()
+        */
+        
+        int numero1 = 3;
+        var numero2 = 5;
+
+        int soma = numero1 + numero2;
+
+        // mostre() # mostra informação na tela
+        System.out.println( "O resultado da operação é " + soma );
     }
 }
